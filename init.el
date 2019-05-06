86b2f53c071ab4b3961ec2e1d4f93e8a1a199722
Kozova1 <mug66kk@gmail.com>
2019-05-06 20:36:47 +0300

    updated cfg

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+
+
 (require 'package)
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
@@ -21,7 +23,17 @@ There are two things you can do about this warning:
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (base16-theme))))
+ '(electric-pair-mode t)
+ '(electric-pair-pairs
+   (quote
+    ((34 . 34)
+     (8216 . 8217)
+     (8220 . 8221)
+     (123 . 125)
+     (39 . 39))))
+ '(package-selected-packages
+   (quote
+    (sr-speedbar helm-swoop paredit auto-complete-clang-async auto-complete web-mode helm base16-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -29,10 +41,15 @@ There are two things you can do about this warning:
  ;; If there is more than one, they won't work right.
  )
 
+(unless (package-installed-p 'indium)
+  (package-install 'indium))
 (use-package base16-theme
 	:ensure t
 	:config
 	(load-theme 'base16-gruvbox-dark-hard t))
+(use-package helm)
+(ac-config-default)
+(global-auto-complete-mode t)
 (global-display-line-numbers-mode)
+(electric-pair-mode)
 (speedbar 1)
-
