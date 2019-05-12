e7b9e6cb8513c42658b54f0f1359529b26d43d0b
Kozova1 <mug66kk@gmail.com>
2019-05-12 20:39:16 +0300

    a

--- init.el
+++ init.el
@@ -1,5 +1,3 @@
-
-
 (require 'package)
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
@@ -33,7 +31,7 @@ There are two things you can do about this warning:
      (39 . 39))))
  '(package-selected-packages
    (quote
-    (sr-speedbar helm-swoop paredit auto-complete-clang-async auto-complete web-mode helm base16-theme))))
+    (spaceline powerline-evil ergoemacs-status arduino-mode sr-speedbar auto-complete web-mode base16-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -41,15 +39,56 @@ There are two things you can do about this warning:
  ;; If there is more than one, they won't work right.
  )
 
-(unless (package-installed-p 'indium)
-  (package-install 'indium))
-(use-package base16-theme
-	:ensure t
-	:config
-	(load-theme 'base16-gruvbox-dark-hard t))
-(use-package helm)
+;; base16-gruvbox-dark-hard-theme.el -- A base16 colorscheme
+
+;;; Commentary:
+;; Base16: (https://github.com/chriskempson/base16)
+
+;;; Authors:
+;; Scheme: Dawid Kurek (dawikur@gmail.com), morhetz (https://github.com/morhetz/gruvbox)
+;; Template: Kaleb Elwert <belak@coded.io>
+
+;;; Code:
+
+(require 'base16-theme)
+
+(defvar base16-gruvbox-dark-hard-colors
+  '(:base00 "#1d2021"
+    :base01 "#3c3836"
+    :base02 "#504945"
+    :base03 "#665c54"
+    :base04 "#bdae93"
+    :base05 "#d5c4a1"
+    :base06 "#ebdbb2"
+    :base07 "#fbf1c7"
+    :base08 "#fb4934"
+    :base09 "#fe8019"
+    :base0A "#fabd2f"
+    :base0B "#b8bb26"
+    :base0C "#8ec07c"
+    :base0D "#83a598"
+    :base0E "#d3869b"
+    :base0F "#d65d0e")
+  "All colors for Base16 Gruvbox dark, hard are defined here.")
+
+;; Define the theme
+(deftheme base16-gruvbox-dark-hard)
+
+;; Add all the faces to the theme
+(base16-theme-define 'base16-gruvbox-dark-hard base16-gruvbox-dark-hard-colors)
+
+;; Mark the theme as provided
+(provide-theme 'base16-gruvbox-dark-hard)
+
+(provide 'base16-gruvbox-dark-hard-theme)
+
+;;; base16-gruvbox-dark-hard-theme.el ends here
+
 (ac-config-default)
 (global-auto-complete-mode t)
 (global-display-line-numbers-mode)
-(electric-pair-mode)
-(speedbar 1)
+(setq sr-speedbar-right-side nil)
+(setq speedbar-use-images t)
+;;(spaceline-emacs-theme)
+(powerline-default-theme)
+(setq powerline-default-separator 'wave)
